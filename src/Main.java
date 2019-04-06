380d872110ac11e73fd63221fc8c88a9156bce29
Nicolas Mikulik <nicolas.mikulik01@gmail.com>
2019-04-06 06:45:16 +0200

    Anonymous inner class and a lambda expression.

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,10 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application implements EventHandler<ActionEvent> {
+public class Main extends Application{
 	
-		Button button;
+		Button button1;
+		Button button2;
 		
 		public static void main(String[] args) {
 			launch(args);
@@ -17,22 +18,34 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 		@Override
 		public void start(Stage primaryStage) throws Exception {
 			primaryStage.setTitle("Title of the window");
-			button = new Button();
-			button.setText("Click me");
-			button.setOnAction(this);
-			button.setText("Click me!");
+			button1 = new Button();
+			button1.setText("Click me?");
+			button2 = new Button("Click me!");
+		
+			button1.setOnAction(new EventHandler<ActionEvent>(){
+				@Override
+				public void handle(ActionEvent event) {
+				System.out.println("I come from an anonymous inner class!");
+			}
+			});
+			
+			button2.setOnAction(e -> {
+				System.out.print("I am written by");
+				System.out.println(" a lambda expression.");
+			});
 			
 			StackPane layout = new StackPane();
-			layout.getChildren().add(button);
+			layout.getChildren().add(button1);
+			layout.getChildren().add(button2);
 			Scene scene = new Scene(layout, 500, 250);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		}
-		
-		@Override
-		public void handle(ActionEvent event) {
-			if (event.getSource() == button) {
-				System.out.println("Mixing GUI with console.");
-			}
-		}
 }
+
+/*@Override
+public void handle(ActionEvent event) {
+	if (event.getSource() == button) {
+		System.out.println("Mixing GUI with console.");
+	}
+}*/
