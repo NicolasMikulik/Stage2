fa2c3ec28a541fc5c09c3232deac0735de5dbc21
Nicolas Mikulik <nicolas.mikulik01@gmail.com>
2019-04-06 11:14:44 +0200

    ComboBox.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
@@ -14,6 +15,7 @@ public class Main extends Application{
 		Stage window;
 		Scene scene;
 		Button button;
+		ComboBox<String> comboBox;
 		
 		public static void main(String[] args) {
 			launch(args);
@@ -22,35 +24,31 @@ public class Main extends Application{
 		@Override
 		public void start(Stage primaryStage) throws Exception {
 			window = primaryStage;
-			window.setTitle("thenewboston");
+			window.setTitle("ComboBox Demo");
 		
-			//Choicebox
-			ChoiceBox<String> choiceBox = new ChoiceBox<>();
-			
-			choiceBox.getItems().add("Apples");
-			choiceBox.getItems().add("Bananas");
-			choiceBox.getItems().addAll("Bacon", "Ham", "Meatballs");
-			choiceBox.setValue("Ham");
-			
-			choiceBox.getSelectionModel().selectedItemProperty().addListener((v, oldValue, newValue) -> System.out.println(newValue));
+			//ComboBox
+			comboBox = new ComboBox<>();
+			comboBox.getItems().addAll("Bacon", "Ham", "Meatballs");
+			comboBox.setPromptText("What is your favourite type of food?");
+			comboBox.setOnAction(e -> System.out.println("User selected: "+comboBox.getValue()));
+			comboBox.setEditable(true);
 			
 			//Button
 			button = new Button("Click me");
-			button.setOnAction(e -> getChoice(choiceBox));
+			button.setOnAction(e -> printMovie());
 			
 			//Layout
 			VBox layout = new VBox(10);
 			layout.setPadding(new Insets(10,10,10,20));
-			layout.getChildren().addAll(choiceBox, button);
+			layout.getChildren().addAll(comboBox, button);
 			
 			scene = new Scene(layout, 300, 250);
 			window.setScene(scene);
 			window.show();
 		}
 		
-		private void getChoice(ChoiceBox<String> choiceBox) {
-			String choice = choiceBox.getValue();
-			System.out.println(choice);
+		private void printMovie() {
+			System.out.println(comboBox.getValue());
 		}
 }
 
